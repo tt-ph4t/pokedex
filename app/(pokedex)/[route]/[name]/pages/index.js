10e1030b473989a6505ff66c8e0e2918793729ef
tt-ph4t <tt.ph4t@gmail.com>
2026-04-19 19:09:39 +0700

    8

--- app/(pokedex)/[route]/[name]/pages/index.js
+++ app/(pokedex)/[route]/[name]/pages/index.js
@@ -997,156 +997,159 @@ export default mapValues(
         ],
       ]);
     },
-    move: ({ context }) => {
-      /** @type Move */
-      const move = context.data;
+    move: {
+      limit: Infinity,
+      render: ({ context }) => {
+        /** @type Move */
+        const move = context.data;
 
-      const contestType = move.contest_type?.name;
-      const ailment = move.meta?.ailment.name;
-      const category = move.meta?.category.name;
+        const contestType = move.contest_type?.name;
+        const ailment = move.meta?.ailment.name;
+        const category = move.meta?.category.name;
 
-      return (
-        <>
-          {table(undefined, [
-            [
-              "The percent value of how likely this move is to be successful.",
-              move.accuracy,
-            ],
-            [
-              "The type of appeal this move gives a Pokémon when used in a contest.",
-              <Link href={`/contest-type/${contestType}`}>
-                {titleCase(contestType)}
-              </Link>,
-            ],
-            [
-              "The effect the move has when used in a contest.",
-              unnamedLink(move.contest_effect?.url),
-            ],
-            [
-              highlighter(
-                "The effect the move has when used in a super contest.",
-                "super contest",
-              ),
-              unnamedLink(move.super_contest_effect?.url),
-            ],
-            [
-              "The type of damage the move inflicts on the target.",
-              <Link href={`/move-damage-class/${move.damage_class.name}`}>
-                {titleCase(move.damage_class.name)}
-              </Link>,
-            ],
-            [
-              "The percent value of how likely it is this moves effect will happen.",
-              move.effect_chance,
-            ],
-            [
-              highlighter(
-                "The generation in which this move was introduced.",
-                "generation",
-              ),
-              <Link href={`/generation/${move.generation.name}`}>
-                {titleCase(move.generation.name)}
-              </Link>,
-            ],
-            [
-              highlighter(
-                "The base power of this move with a value of 0 if it does not have a base power.",
-                "The base power",
-              ),
-              move.power,
-            ],
-            [
-              highlighter(
-                "Power points. The number of times this move can be used.",
-                "Power points",
-              ),
-              move.pp,
-            ],
-            [
-              "A value between -8 and 8. Sets the order in which moves are executed during battle.",
-              move.priority,
-            ],
-            [
-              highlighter(
-                "The type of target that will receive the effects of the attack.",
-                "target",
-              ),
-              <Link href={`/move-target/${move.target.name}`}>
-                {titleCase(move.target.name)}
-              </Link>,
-            ],
-            [
-              highlighter("The elemental type of this move.", "type"),
-              <Link href={`/type/${move.type.name}`}>
-                {titleCase(move.type.name)}
-              </Link>,
-            ],
-          ])}
-          {tabs({
-            contest_combos: tabs(
-              mapValues(move?.contest_combos ?? {}, (value) =>
-                tabs(
-                  mapValues(value, (value) =>
-                    table.pagination(value, {
-                      renderRows: ({ context }) => [
-                        <Link href={`/move/${context.name}`}>
-                          {titleCase(context.name)}
-                        </Link>,
-                      ],
-                    }),
+        return (
+          <>
+            {table(undefined, [
+              [
+                "The percent value of how likely this move is to be successful.",
+                move.accuracy,
+              ],
+              [
+                "The type of appeal this move gives a Pokémon when used in a contest.",
+                <Link href={`/contest-type/${contestType}`}>
+                  {titleCase(contestType)}
+                </Link>,
+              ],
+              [
+                "The effect the move has when used in a contest.",
+                unnamedLink(move.contest_effect?.url),
+              ],
+              [
+                highlighter(
+                  "The effect the move has when used in a super contest.",
+                  "super contest",
+                ),
+                unnamedLink(move.super_contest_effect?.url),
+              ],
+              [
+                "The type of damage the move inflicts on the target.",
+                <Link href={`/move-damage-class/${move.damage_class.name}`}>
+                  {titleCase(move.damage_class.name)}
+                </Link>,
+              ],
+              [
+                "The percent value of how likely it is this moves effect will happen.",
+                move.effect_chance,
+              ],
+              [
+                highlighter(
+                  "The generation in which this move was introduced.",
+                  "generation",
+                ),
+                <Link href={`/generation/${move.generation.name}`}>
+                  {titleCase(move.generation.name)}
+                </Link>,
+              ],
+              [
+                highlighter(
+                  "The base power of this move with a value of 0 if it does not have a base power.",
+                  "The base power",
+                ),
+                move.power,
+              ],
+              [
+                highlighter(
+                  "Power points. The number of times this move can be used.",
+                  "Power points",
+                ),
+                move.pp,
+              ],
+              [
+                "A value between -8 and 8. Sets the order in which moves are executed during battle.",
+                move.priority,
+              ],
+              [
+                highlighter(
+                  "The type of target that will receive the effects of the attack.",
+                  "target",
+                ),
+                <Link href={`/move-target/${move.target.name}`}>
+                  {titleCase(move.target.name)}
+                </Link>,
+              ],
+              [
+                highlighter("The elemental type of this move.", "type"),
+                <Link href={`/type/${move.type.name}`}>
+                  {titleCase(move.type.name)}
+                </Link>,
+              ],
+            ])}
+            {tabs({
+              contest_combos: tabs(
+                mapValues(move?.contest_combos ?? {}, (value) =>
+                  tabs(
+                    mapValues(value, (value) =>
+                      table.pagination(value, {
+                        renderRows: ({ context }) => [
+                          <Link href={`/move/${context.name}`}>
+                            {titleCase(context.name)}
+                          </Link>,
+                        ],
+                      }),
+                    ),
                   ),
                 ),
               ),
-            ),
-            learned_by_pokemon: table.pagination(move.learned_by_pokemon, {
-              renderRows: ({ context }) => [
-                <Link href={`/pokemon/${context.name}`}>
-                  {titleCase(context.name)}
-                </Link>,
-              ],
-            }),
-            meta: table(
-              undefined,
-              Object.entries({
-                ailment: (
-                  <Link href={`/move-ailment/${ailment}`}>
-                    {titleCase(ailment)}
-                  </Link>
-                ),
-                ailment_chance: move.meta?.ailment_chance,
-                category: (
-                  <Link href={`/move-category/${category}`}>
-                    {titleCase(category)}
-                  </Link>
-                ),
-                crit_rate: move.meta?.crit_rate,
-                drain: move.meta?.drain,
-                flinch_chance: move.meta?.flinch_chance,
-                healing: move.meta?.healing,
-                max_hits: move.meta?.max_hits,
-                max_turns: move.meta?.max_turns,
-                min_hits: move.meta?.min_hits,
-                min_turns: move.meta?.min_turns,
-                stat_chance: move.meta?.stat_chance,
-              }).map(([key, value]) => [titleCase(key), value]),
-            ),
-            stat_changes: table.pagination(move.stat_changes, {
-              renderRows: ({ context }) => [
-                <Link href={`/stat/${context.stat.name}`}>
-                  {titleCase(context.stat.name)}
-                </Link>,
-                context.change,
-              ],
-              thead: [undefined, "change"],
-            }),
-            ...internalTabs.effectChanges(move.effect_changes),
-            ...internalTabs.effectEntries(move.effect_entries),
-            ...internalTabs.flavorTextEntries(move.flavor_text_entries),
-            ...internalTabs.machines(move.machines),
-            ...internalTabs.names(move.names),
-          })}
-        </>
-      );
+              learned_by_pokemon: table.pagination(move.learned_by_pokemon, {
+                renderRows: ({ context }) => [
+                  <Link href={`/pokemon/${context.name}`}>
+                    {titleCase(context.name)}
+                  </Link>,
+                ],
+              }),
+              meta: table(
+                undefined,
+                Object.entries({
+                  ailment: (
+                    <Link href={`/move-ailment/${ailment}`}>
+                      {titleCase(ailment)}
+                    </Link>
+                  ),
+                  ailment_chance: move.meta?.ailment_chance,
+                  category: (
+                    <Link href={`/move-category/${category}`}>
+                      {titleCase(category)}
+                    </Link>
+                  ),
+                  crit_rate: move.meta?.crit_rate,
+                  drain: move.meta?.drain,
+                  flinch_chance: move.meta?.flinch_chance,
+                  healing: move.meta?.healing,
+                  max_hits: move.meta?.max_hits,
+                  max_turns: move.meta?.max_turns,
+                  min_hits: move.meta?.min_hits,
+                  min_turns: move.meta?.min_turns,
+                  stat_chance: move.meta?.stat_chance,
+                }).map(([key, value]) => [titleCase(key), value]),
+              ),
+              stat_changes: table.pagination(move.stat_changes, {
+                renderRows: ({ context }) => [
+                  <Link href={`/stat/${context.stat.name}`}>
+                    {titleCase(context.stat.name)}
+                  </Link>,
+                  context.change,
+                ],
+                thead: [undefined, "change"],
+              }),
+              ...internalTabs.effectChanges(move.effect_changes),
+              ...internalTabs.effectEntries(move.effect_entries),
+              ...internalTabs.flavorTextEntries(move.flavor_text_entries),
+              ...internalTabs.machines(move.machines),
+              ...internalTabs.names(move.names),
+            })}
+          </>
+        );
+      },
     },
     "move-ailment": ({ context }) => {
       /** @type MoveAilment */
